675ae23115bc5dfce86fd5f700903f1079dd4095
victorgis <essangvictor@gmail.com>
2023-01-14 18:31:46 +0100

    initial commit

--- controllers/facilitiesControllers.js
+++ controllers/facilitiesControllers.js
@@ -1,25 +1,39 @@
 
-
+// @desc    Get facilities
+// @route   GET /api/facilities
+// @access  Private
 const getFacilities = (req, res) => {
     res.status(200).json({message: "Get goals"})
 }
 
+// @desc    Post goals
+// @route   POST /api/facilities
+// @access  Private
 const setFacility = (req, res) => {
     res.status(200).json({message: "Set goals"})
 }
 
+// @desc    Put goals
+// @route   PUT /api/facilities
+// @access  Private
+const updateFacility = (req, res) => {
+    res.status(200).json({message: "update goals"})
+}
+
+// @desc    Delete goals
+// @route   DELETE /api/facilities
+// @access  Private
 const deleteFacility = (req, res) => {
     res.status(200).json({message: "delete goals"})
 }
 
-const updateFacility = (req, res) => {
-    res.status(200).json({message: "update goals"})
-}
+
 
 
 module.exports = {
     getFacilities,
     setFacility,
-    deleteFacility,
-    updateFacility
+    updateFacility,
+    deleteFacility
+    
 }
